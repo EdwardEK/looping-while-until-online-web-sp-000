4ac581f22ac2eb291a0daa24655add5965afb978
Edward Kane <edwarde.kane@gmail.com>
2020-02-19 15:09:51 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,4 +1,5 @@
 def using_until
+<<<<<<< HEAD
   require_relative "../lib/until.rb"
   counter=0
   levitation_force = 6
@@ -8,3 +9,11 @@ until counter==10
     break
 end
 puts "And the Loop is Done"
+=======
+  counter=0
+  levitation_force = 6
+until counter==10
+  puts "wingardium Leviosa until our levitation fore equals 10"
+    counter += 1
+end
+>>>>>>> a5110907b6a3b022bc13e9863e6f293a6bac0ca8
