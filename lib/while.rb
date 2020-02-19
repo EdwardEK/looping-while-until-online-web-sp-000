7a8fe13611a988dd5a5af8bc72978c256b8b8980
Edward Kane <edwarde.kane@gmail.com>
2020-02-19 15:13:42 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,20 +1,15 @@
 def using_while
-<<<<<<< HEAD
+
   require_relative "../lib/while.rb"
-=======
->>>>>>> a5110907b6a3b022bc13e9863e6f293a6bac0ca8
+
   counter=0
   levitation_force = 6
 
   while counter <10
-<<<<<<< HEAD
+
     puts "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10"
     counter += 1
-    break
+  
 end
-puts "And the Loop is Done"
-=======
-    puts "wingardium Leviosa until the leviatation force is incremented to 10"
-    counter += 1
-end
->>>>>>> a5110907b6a3b022bc13e9863e6f293a6bac0ca8
+
+
